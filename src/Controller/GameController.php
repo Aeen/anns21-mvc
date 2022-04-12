0709f4dd6f9789d2ae8c01aa1ff6975e13d890b3
Anna Nilsson <anns21@student.bth.se>
2022-04-12 14:34:04 +0200

    Spelplan fixad

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -27,11 +27,33 @@ class GameController extends AbstractController
         return $this->render('game\gamedoc.html.twig');
     }
 
-        /**
+    /**
     * @Route("/game/play", name="game-play")
     */
     public function gamePlay(): Response
     {
-        return $this->render('game\play.html.twig');
+        return $this->render('game\game.html.twig');
+    }
+
+    /**
+     * @Route("/game/playing", name="game-playing")
+     */
+    public function card(): response
+    {
+        $cardArray = [];
+        $cards = new \App\Card\Deck();
+        $deck = $cards->getCards();
+
+        for ($i = 0; $i < count($deck); $i++) {
+            array_push($cardArray, $deck[$i]->toString());
+        }
+
+        $data = [
+            'title' => 'Card',
+            'dealerHand' => $cardArray,
+            'playerHand' => $cardArray
+        ];
+
+        return $this->render('game\play.html.twig', $data);
     }
 }
