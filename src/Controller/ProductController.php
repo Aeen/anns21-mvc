369bf387ff384f9da002f7ddb3510d25a3a4cb7f
Anna Nilsson <anns21@student.bth.se>
2022-04-26 09:38:19 +0200

    övningen färdig

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -7,6 +7,8 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Product;
 use Doctrine\Persistence\ManagerRegistry;
+use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
 
 class ProductController extends AbstractController
 {
@@ -38,6 +40,77 @@ class ProductController extends AbstractController
         $entityManager->flush();
 
         return new Response('Saved new product with id '.$product->getId());
+    }
+
+    /**
+     * @Route("/product/show", name="product_show_all")
+     */
+    public function showAllProduct(
+        EntityManagerInterface $entityManager
+    ): Response {
+    $products = $entityManager
+        ->getRepository(Product::class)
+        ->findAll();
+
+    return $this->json($products);
+    }
+
+    /**
+     * @Route("/product/show/{id}", name="product_by_id")
+     */
+    public function showProductById(
+        ProductRepository $productRepository,
+        int $id
+    ): Response {
+        $products = $productRepository
+            ->find($id);
+
+        return $this->json($products);
+    }
+
+    /**
+     * @Route("/product/delete/{id}", name="product_delete_by_id")
+     */
+    public function deleteProductById(
+        ManagerRegistry $doctrine,
+        int $id
+    ): Response {
+        $entityManager = $doctrine->getManager();
+        $product = $entityManager->getRepository(Product::class)->find($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException(
+                'No product found for id '.$id
+            );
+        }
+
+        $entityManager->remove($product);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('product_show_all');
+    }
+
+    /**
+     * @Route("/product/update/{id}/{value}", name="product_update")
+     */
+    public function updateProduct(
+        ManagerRegistry $doctrine,
+        int $id,
+        int $value
+    ): Response {
+        $entityManager = $doctrine->getManager();
+        $product = $entityManager->getRepository(Product::class)->find($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException(
+                'No product found for id '.$id
+            );
+        }
+
+        $product->setValue($value);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('product_show_all');
     }
 }
 
