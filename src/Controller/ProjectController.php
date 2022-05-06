f94f3db142d8740bfa8b30ff305380723932a533
Anna Nilsson <anns21@student.bth.se>
2022-05-06 17:15:26 +0200

    Projektsida skapad

---
+++ src/Controller/ProjectController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ProjectController extends AbstractController
+{
+    /**
+     * @Route("/project", name="project")
+     */
+    public function project(): Response
+    {
+        return $this->render('project\index.html.twig');
+    }
+}
